88b7e8100722b162a9a548b73e5946e38a4adb11
Abhishek <AbhishekIIITD>
2024-12-14 23:50:49 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { EventProvider } from "../context/EventContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <EventProvider>
+      <Component {...pageProps} />
+    </EventProvider>
+  );
 }
